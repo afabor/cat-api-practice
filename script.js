f537327e6108cc2a112eb07088a369e1605fd720
Your Name <you@example.com>
2024-03-12 11:56:22 +0000

    cat images done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const fetchImage = ()=>{
-    let image = document.getElementById('cat-image');
-    fetch('https://api.thecatapi.com/v1/images/search').then(res) => {
-        return res.json()
-    }
-}
+const fetchImage = () => {
+  let image = document.getElementById("cat-img");
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log("cat images", json);
+      image.src = json[0].url;
+    });
+};
